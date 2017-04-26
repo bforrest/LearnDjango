6c9d6242066a66ad6ac0c33b3a29c721a45107ba
Barry Forrest <bforrest30@gmail.com>
2017-04-25 22:30:48 -0500

    added models

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResonse("Heloo, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.")
